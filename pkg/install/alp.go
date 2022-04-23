156b6baa3264c28fe49ac3a2dd7a78db1410851c
ShotaKitazawa <skitazawa1121@gmail.com>
2022-04-23 17:35:16 +0900

    WIP

--- pkg/install/alp.go
+++ pkg/install/alp.go
@@ -12,14 +12,21 @@ func (i *Installer) Alp(ctx context.Context, version string) error {
 		// get release
 		// get latest tag
 	}
-	command := fmt.Sprintf(`
-curl -sL https://github.com/tkuchiki/alp/releases/download/%s/alp_linux_amd64.zip -o /tmp/alp.zip;
-unzip /tmp/alp.zip -d /usr/local/bin;
-`, version)
+	command := fmt.Sprintf(
+		"curl -sL https://github.com/tkuchiki/alp/releases/download/%s/alp_linux_amd64.zip -o /tmp/alp.zip",
+		version)
 	stdout, stderr, err := i.runCommand(ctx, "", command)
 	if err != nil {
 		return myerrors.NewErrorCommandExecutionFailed(stderr)
 	}
 	i.log.Debug(stdout.String())
+
+	command = "unzip -f /tmp/alp -d /usr/local/bin"
+	stdout, stderr, err = i.runCommand(ctx, "", command)
+	if err != nil {
+		return myerrors.NewErrorCommandExecutionFailed(stderr)
+	}
+	i.log.Debug(stdout.String())
+
 	return nil
 }
