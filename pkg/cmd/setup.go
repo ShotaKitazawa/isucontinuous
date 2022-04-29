ddee1c6ec4bedfd0aea0ed0b322a84d89f3ae817
kanata <skitazawa1121@gmail.com>
2022-04-29 11:43:36 +0900

    refactoring (#8)

--- pkg/cmd/setup.go
+++ pkg/cmd/setup.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ShotaKitazawa/isu-continuous/pkg/config"
 	"github.com/ShotaKitazawa/isu-continuous/pkg/install"
+	"github.com/ShotaKitazawa/isu-continuous/pkg/localrepo"
 	"github.com/ShotaKitazawa/isu-continuous/pkg/shell"
 )
 
@@ -21,8 +22,13 @@ func RunSetup(conf ConfigSetup) error {
 	if err != nil {
 		return err
 	}
+	// Attach local isucon-repo
+	repo, err := localrepo.AttachLocalRepo(logger, exec.New(), conf.LocalRepoPath)
+	if err != nil {
+		return err
+	}
 	// load isucontinuous.yaml
-	isucontinuous, err := config.Load(conf.LocalRepoPath, isucontinuousFilename)
+	isucontinuous, err := repo.LoadConf(isucontinuousFilename)
 	if err != nil {
 		return err
 	}
@@ -40,11 +46,18 @@ func RunSetup(conf ConfigSetup) error {
 		}
 		installers[host.Host] = install.NewInstaller(logger, s)
 	}
-
-	return runSetup(conf, ctx, logger, isucontinuous, installers)
+	return runSetup(conf, ctx, logger, repo, installers)
 }
 
-func runSetup(conf ConfigSetup, ctx context.Context, logger *zap.Logger, isucontinuous *config.Config, installers map[string]*install.Installer) error {
+func runSetup(
+	conf ConfigSetup, ctx context.Context, logger *zap.Logger,
+	repo localrepo.LocalRepoIface, installers map[string]*install.Installer,
+) error {
+	// load isucontinuous.yaml
+	isucontinuous, err := repo.LoadConf(isucontinuousFilename)
+	if err != nil {
+		return err
+	}
 	return perHostExec(logger, ctx, isucontinuous.Hosts, func(ctx context.Context, host config.Host) error {
 		installer := installers[host.Host]
 		// install docker
