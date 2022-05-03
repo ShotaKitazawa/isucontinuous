9bc1e6f1f49059facb7cd22e4a62289589cabddd
ShotaKitazawa <skitazawa1121@gmail.com>
2022-05-03 21:58:31 +0900

    fix setup command: using unzip command during to install alp

--- pkg/usecases/install/alp.go
+++ pkg/usecases/install/alp.go
@@ -1,13 +1,8 @@
 package install
 
 import (
-	"archive/zip"
 	"context"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 
 	"go.uber.org/zap"
 
@@ -31,48 +26,15 @@ func (i *Installer) Alp(ctx context.Context, version string) error {
 	command := fmt.Sprintf(
 		"curl -sL https://github.com/tkuchiki/alp/releases/download/%s/alp_linux_amd64.zip -o /tmp/alp.zip",
 		version)
-	stdout, stderr, err := i.shell.Exec(ctx, "", command)
-	if err != nil {
+	if _, stderr, err := i.shell.Exec(ctx, "", command); err != nil {
 		return myerrors.NewErrorCommandExecutionFailed(stderr)
 	}
-	i.log.Debug(stdout.String(), zap.String("host", i.shell.Host()))
+	i.log.Debug("downloaded to /tmp/alp.zip", zap.String("host", i.shell.Host()))
 
-	if err := i.unzip("/tmp/alp.zip", "/usr/local/bin/"); err != nil {
+	if _, stderr, err := i.shell.Exec(ctx, "", "unzip /tmp/alp.zip -d /usr/local/bin/"); err != nil {
 		return myerrors.NewErrorCommandExecutionFailed(stderr)
 	}
 
 	i.log.Info("... installed alp!", zap.String("host", i.shell.Host()))
-	return nil
-}
-
-func (i *Installer) unzip(src, dest string) error {
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-	for _, f := range r.File {
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer rc.Close()
-		if f.FileInfo().IsDir() {
-			path := filepath.Join(dest, f.Name)
-			if err := os.MkdirAll(path, f.Mode()); err != nil {
-				return err
-			}
-		} else {
-			buf := make([]byte, f.UncompressedSize)
-			_, err = io.ReadFull(rc, buf)
-			if err != nil {
-				return err
-			}
-			path := filepath.Join(dest, f.Name)
-			if err = ioutil.WriteFile(path, buf, f.Mode()); err != nil {
-				return err
-			}
-		}
-	}
 	return nil
 }
